8aaf73704b2ba139c0a8f026c58392a7705ad094
SanderSpies <sandermail@gmail.com>
2014-11-18 08:33:16 +0100

    Fix webpack example

--- examples/webpack/index.js
+++ examples/webpack/index.js
@@ -5,30 +5,34 @@
 
 require('normalize.css/normalize.css');
 
-var React       = require('react');
-var ReactStyle  = require('react-style');
-var Icon        = require('react-fa');
-var Button      = require('./Button');
-var ButtonGroup = require('./ButtonGroup');
+var ReactStyle   = require('react-style');
+var React        = require('react');
+var Icon         = require('react-fa');
+var Button       = require('./Button');
+var ButtonStyles = require('./ButtonStyles');
+var ButtonGroup  = require('./ButtonGroup');
 
 var TextAlignSwitcher = React.createClass({
 
   render() {
+    var props = this.props;
+    var textAlign = props.textAlign;
+    var onTextAlign = props.onTextAlign;
     return (
-      <ButtonGroup>
+      <ButtonGroup styles={props.styles}>
         <Button
-          active={this.props.textAlign === 'left'}
-          onClick={this.props.onTextAlign.bind(null, 'left')}>
+          active={textAlign === 'left'}
+          onClick={onTextAlign.bind(null, 'left')}>
           <Icon name="align-left" /> Left
         </Button>
         <Button
-          active={this.props.textAlign === 'center'}
-          onClick={this.props.onTextAlign.bind(null, 'center')}>
+          active={textAlign === 'center'}
+          onClick={onTextAlign.bind(null, 'center')}>
           <Icon name="align-center" /> Center
         </Button>
         <Button
-          active={this.props.textAlign === 'right'}
-          onClick={this.props.onTextAlign.bind(null, 'right')}>
+          active={textAlign === 'right'}
+          onClick={onTextAlign.bind(null, 'right')}>
           <Icon name="align-right" /> Right
         </Button>
       </ButtonGroup>
@@ -36,51 +40,55 @@ var TextAlignSwitcher = React.createClass({
   }
 });
 
-var Application = React.createClass({
+var ApplicationStyles = {
 
-  style: ReactStyle({
+  normalStyle: ReactStyle({
     backgroundColor: 'white',
     fontSize: '10pt',
     padding: '1em',
-    children: {
-      marginRight: '0.5em',
-    },
-    lastChild: {
-      marginRight: 0
-    }
-  }),
+    margin: 10
+  }, 'Application_normalStyle'),
+
+  childStyle: ReactStyle({
+    marginRight: '0.5em'
+  }, 'Application_childStyle'),
+
+  lastChildStyle: ReactStyle({
+    marginRight: 0
+  }, 'Application_lastChildStyle')
+
+};
+
+class Application {
+
+  getInitialState() {
+    return {
+      textAlign: 'left'
+    };
+  }
 
   render() {
-    return ReactStyle.style(this.style,
-      <div>
+    return (
+      <div styles={ApplicationStyles.normalStyle}>
         <h1 styles={ReactStyle({textAlign: this.state.textAlign})}>Application</h1>
-        <Button styles={Button.styles.success}>
+        <Button styles={[ButtonStyles.success]}>
           <Icon name="cog" /> OK
         </Button>
-        <Button styles={Button.styles.error}>
+        <Button styles={[ApplicationStyles.childStyle, ButtonStyles.error]}>
           <Icon name="remove" /> Cancel
         </Button>
         <TextAlignSwitcher
-          textAlign={this.state.textAlign}
-          onTextAlign={this.onTextAlign}
-          />
+          styles={ApplicationStyles.lastChild}
+          onTextAlign={(textAlign) => this.setState({textAlign: textAlign})}
+        />
       </div>
     );
-  },
-
-  getInitialState() {
-    return {textAlign: 'left'};
-  },
-
-  onTextAlign(textAlign) {
-    this.setState({textAlign});
   }
 
-});
 
-if (typeof window !== 'undefined') {
-  ReactStyle.inject();
-  React.render(<Application />, document.getElementById('app'));
 }
 
-module.exports = Application;
+if (typeof window !== 'undefined') {
+  var ApplicationTag = React.createClass(Application.prototype);
+  React.render(<ApplicationTag />, document.getElementById('app'));
+}
